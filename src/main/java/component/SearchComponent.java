1a0ca3f7fa8a9ba5186f5f714b62119e41d0a749
OldAtaraxia <aqwanghaoxuan@163.com>
2021-12-13 17:35:07 +0800

    feat: add bilibili search

---
+++ src/main/java/component/SearchComponent.java
@@ -0,0 +1,11 @@
+package component;
+
+public class BilibiliSearchComponent {
+    public static void registerSearchComponent() {
+
+    }
+
+    public static void search(String Rssurl) {
+
+    }
+}
